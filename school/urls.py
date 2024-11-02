f72aca6537e0abf3000d2b32e5ff9b06ab14ccac
anti <pixell_tm@bk.ru>
2024-11-02 14:40:20 +0300

    fix home-page and registe-login

--- school/urls.py
+++ school/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from users.views import RegisterView, LoginView
+from main.views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
 ]
